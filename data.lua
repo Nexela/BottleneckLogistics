852436937e9c5d7302c3ba52b55758883d3f8fa0
Nexela <nexela@gmail.com>
2017-04-28 10:19:14 -0400

    really fixed

--- data.lua
+++ data.lua
@@ -1,6 +1,5 @@
 local normal = table.deepcopy(data.raw["storage-tank"]["bottleneck-stoplight"])
 normal.name = "bottleneck-stoplight-scaled"
-normal.render_layer = "explosion"
 for _, dir in pairs(normal.pictures.picture) do
     dir.scale = .45
     dir.shift = {dir.shift[1]-.1, dir.shift[2]+.1}
@@ -8,9 +7,39 @@ end
 
 local high = table.deepcopy(data.raw["storage-tank"]["bottleneck-stoplight-high"])
 high.name = "bottleneck-stoplight-high-scaled"
-for _, dir in pairs(normal.pictures.picture) do
+for _, dir in pairs(high.pictures.picture) do
     dir.scale = .45
     dir.shift = {dir.shift[1]-.1, dir.shift[2]+.1}
 end
-data.raw["simple-entity-with-force"]["simple-entity-with-force"].render_layer = "explosion"
+
+--Prepare for conversion to simple-entity!
+--data.raw["simple-entity-with-force"]["simple-entity-with-force"].render_layer = "explosion"
 data:extend({normal, high})
+--
+-- tile
+-- tile-transition
+-- resource
+-- decorative
+-- remnants
+-- floor
+-- transport-belt-endings
+-- corpse
+-- floor-mechanics
+-- item
+-- lower-object
+-- object
+-- higher-object-above
+-- higher-object-under
+-- wires
+-- lower-radius-visualization
+-- radius-visualization
+-- entity-info-icon
+-- explosion
+-- projectile
+-- smoke
+-- air-object
+-- air-entity-info-con
+-- light-effect
+-- selection-box
+-- arrow
+-- cursor
